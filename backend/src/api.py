2679425745d37093511714216e2ffa0d0879f730
ademuyiwa stephanie <stephanieademuyiwa1@gmail.com>
2022-06-29 13:07:20 +0100

    the backend error

--- backend/src/api.py
+++ backend/src/api.py
@@ -118,31 +118,31 @@ def not_found(error):
 '''
 
 
-# @app.errorhandler(400)
-# def bad_request(error):
-#     return jsonify({
-#         "success": False,
-#         "error": 400,
-#         "message": "Bad Request"
-#     }), 400
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "Bad Request"
+    }), 400
 
 
-# @app.errorhandler(405)
-# def method_not_allowed(error):
-#     return jsonify({
-#         "success": False,
-#         "error": 405,
-#         "message": "Method Not Allowed"
-#     }), 405
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return jsonify({
+        "success": False,
+        "error": 405,
+        "message": "Method Not Allowed"
+    }), 405
 
 
-# @app.errorhandler(500)
-# def internal_server_error(error):
-#     return jsonify({
-#         "success": False,
-#         "error": 500,
-#         "message": "Internal Server Error"
-#     }), 500
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({
+        "success": False,
+        "error": 500,
+        "message": "Internal Server Error"
+    }), 500
 
 
 '''
